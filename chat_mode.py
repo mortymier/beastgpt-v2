2f95bae76c4c84eab0fbdb5b561bccdbab276b75
Mortymier <sanielmg@gmail.com>
2026-05-06 01:20:17 +0800

    Create separate file for chat mode

---
+++ chat_mode.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def render_chat():
+    st.markdown(" ")
+    st.markdown("<div class='divider'></div>", unsafe_allow_html=True)
+    st.markdown("<h1 class='mode-header'> DESCRIBE YOUR BATTLE! </h1>", unsafe_allow_html=True)
+    st.markdown("<div class='divider'></div>", unsafe_allow_html=True)
+    st.markdown("#")
+    st.write("Chat page - content coming soon.")
